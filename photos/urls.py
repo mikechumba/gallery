ec05e3692260b22cc19317582dd3c45496d87b20
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-01 21:42:13 +0300

    style image display

--- photos/urls.py
+++ photos/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
 
 from . import views
@@ -5,3 +7,6 @@ from . import views
 urlpatterns = [
    path('', views.home, name="photos'home")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Image,Category,Location
 
 def home(request):
-   images = Image.objects.order_by('-pk')[:5]
+   images = Image.objects.order_by('-pk')[:10]
 
    context = {
       'images': images
