9e1d25e76e680e6e7ee731dccf1f916480b7ac10
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-05 08:45:52 +0300

    redefine image url

--- photos/tests.py
+++ photos/tests.py
@@ -2,7 +2,18 @@ from django.test import TestCase
 
 from .models import Image,Location,Category
 
+
+def create_location_instance():
+   new_location = Location(location_name='Nairobi')
+   return new_location
+
+def create_image_instance():
+   
+
 class ImageTest(TestCase):
 
-   def create_test(self):
-      image 
+   def setUp(self):
+      self.image = Image(image = 'default.jpg',image_name = 'Default Picture',description = 'Some picture',locale = 'Nairobi', category = 'Misc')
+
+   def test_instance(self):
+      self.assertTrue(isinstance(self.image,Image))
