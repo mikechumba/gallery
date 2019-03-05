06040c8afd013fb1c28db9ba6768e7bef415e334
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-05 09:47:20 +0300

    test image instance

--- photos/tests.py
+++ photos/tests.py
@@ -7,13 +7,20 @@ def create_location_instance():
    new_location = Location(location_name='Nairobi')
    return new_location
 
-def create_image_instance():
-   
+def create_image_instance(location,cat):
+   new_image = Image(image = 'default.jpg',image_name = 'Default Picture',description = 'Some picture',locale = location, category = cat)
+   return new_image
+
+def create_category_instance():
+   new_category = Category(category_name = "Travel")
+   return new_category
 
 class ImageTest(TestCase):
 
    def setUp(self):
-      self.image = Image(image = 'default.jpg',image_name = 'Default Picture',description = 'Some picture',locale = 'Nairobi', category = 'Misc')
+      location = create_location_instance()
+      category = create_category_instance()
+      self.new_image = create_image_instance(location,category)
 
    def test_instance(self):
       self.assertTrue(isinstance(self.image,Image))
